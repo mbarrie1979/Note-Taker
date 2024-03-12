bd6968489c9f69bc4c87f64bbdc10e9dd8d48d0b
Mark Barrie <mark.a.barrie@gmail.com>
2024-03-12 13:45:35 -0400

    server is functioning

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+app.listen(PORT, (err) =>
+    err ? console.error(err) : console.log(`listening to port ${PORT}`));
