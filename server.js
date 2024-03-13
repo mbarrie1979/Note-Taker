f1be8672bdd6c04e0d92152715926b4a2dea07a5
Mark Barrie <mark.a.barrie@gmail.com>
2024-03-13 19:38:01 -0400

    added home route

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
